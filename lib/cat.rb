cdac40eeaf0fd3fb9274c75f1b8650a9cbb93282
ShyamliC <shyamli.channa@gmail.com>
2018-07-13 18:30:13 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,12 +1,14 @@
 class Cat
   # code goes here
   
+  attr_reader :name
+  
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
   end
   
-  attr_reader :name
+  
   
 end
 
